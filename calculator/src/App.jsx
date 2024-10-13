cf5a3b4d8e9674e014c7b6c7f0e8749c80864526
Sourav Tanwar <souravtanwar30@gmail.com>
2024-10-13 20:43:21 +0530

    setup: react setup done for timmer

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 
+const initialState =  {count: 0} ;
+
+function filterreducer(state, {type}) {
+  switch (type) {
+    case "increment":
+      return {count: state.count + 1} ;
+    case "decrement":
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
   
+  const [state, filterdispatch] = useReducer(filterreducer,initialState)
+
 
   return (
     <div style={{display:"flex", justifyContent: "center", alignItems: "center", flexDirection:"column" }}>
-      
+      Count: {state.count}
+      <button onClick={() => filterdispatch({ type: "increment" })}>+</button>
+      <button onClick={() => filterdispatch({ type: "decrement" })}>-</button>
     </div>
   )
 }
